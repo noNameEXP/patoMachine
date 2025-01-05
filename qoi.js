e41fc5e9569496bc6cf85825ea8e38b4c2b35381
noName <132767802+noNameEXP@users.noreply.github.com>
2025-01-05 16:28:03 +0100

    Update qoi.js

--- qoi.js
+++ qoi.js
@@ -2,7 +2,13 @@
 
 const QOI = {};
 
-QOI.decode = require('./decode');
-QOI.encode = require('./encode');
-
-module.exports = QOI;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    QOI.decode = require('./decode');
+    QOI.encode = require('./encode');
+    module.exports = QOI;
+} else {
+    // Ensure these functions are defined globally for browser usage
+    QOI.decode = window.decode;
+    QOI.encode = window.encode;
+    window.QOI = QOI;
+}
